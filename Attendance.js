145c47e7a8e3eefc42fd2928309a35948c654bcb
mrhunter4037 <abdo.sameer.x2014@gmail.com>
2022-07-17 19:41:18 +0300

    date in  js

---
+++ Attendance.js
@@ -0,0 +1,6 @@
+let todaydate= document.getElementById("date-heading");
+todaydate.innerHTML=`Attendance for
+${new Date().getMonth() + 1}/${new Date().getDate()}/${new Date().getFullYear()}`;
+
+
+students=[{id: 0, firstname: "lana", lastname: "qawasmy", atte: "empty"}];
